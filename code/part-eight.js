b1af198b35fc1eafa1f9bd3bca99f4dc27ecdee0
lamhotsimamora <lamhotsimamora36@gmail.com>
2017-08-31 01:04:12 +0700

    #3

--- code/part-eight.js
+++ code/part-eight.js
@@ -11,3 +11,173 @@ _keyArrow(function(){
 _keyCustom(function(){
     _printTo("display_key","BACKSPACE");
 },_keyCode.backspace);
+
+_keyCustom(function(){
+	_printTo("key_name","CTRL");
+	_printTo("key_code",_keyCode.ctrl);
+},_keyCode.ctrl);
+
+_keyCustom(function(){
+	_printTo("key_name","SPACE");
+	_printTo("key_code",_keyCode.space_bar);
+},_keyCode.space_bar); 
+
+_keyCustom(function(){
+	_printTo("key_name","CLEAR");
+	_printTo("key_code",_keyCode.clear);
+},_keyCode.clear);
+
+_keyCustom(function(){
+	_printTo("key_name","TAB");
+	_printTo("key_code",_keyCode.tab);
+},_keyCode.tab);
+
+_keyCustom(function(){
+	_printTo("key_name","CAPS LOCK");
+	_printTo("key_code",_keyCode.caps_lock);
+},_keyCode.caps_lock);
+
+_keyCustom(function(){
+	_printTo("key_name","SHIFT");
+	_printTo("key_code",_keyCode.shift);
+},_keyCode.shift);
+ 
+_keyCustom(function(){
+	_printTo("key_name","ALT");
+	_printTo("key_code",_keyCode.alt);
+},_keyCode.alt);
+
+_keyCustom(function(){
+	_printTo("key_name","ESCAPE");
+	_printTo("key_code",_keyCode.escape);
+},_keyCode.escape);
+
+_keyCustom(function(){
+	_printTo("key_name","HOME");
+	_printTo("key_code",_keyCode.home);
+},_keyCode.home);
+ 
+_keyCustom(function(){
+	_printTo("key_name","INSERT");
+	_printTo("key_code",_keyCode.insert);
+},_keyCode.insert);
+
+_keyCustom(function(){
+	_printTo("key_name","DELETE");
+	_printTo("key_code",_keyCode.delete);
+},_keyCode.delete);
+
+_keyCustom(function(){
+	_printTo("key_name","END");
+	_printTo("key_code",_keyCode.end);
+},_keyCode.end);
+
+_keyCustom(function(){
+	_printTo("key_name","ENTER");
+	_printTo("key_code",_keyCode.enter);
+},_keyCode.enter);
+
+_keyCustom(function(){
+	_printTo("key_name","F1");
+	_printTo("key_code",_keyCode.f1);
+},_keyCode.f1);
+
+_keyCustom(function(){
+	_printTo("key_name","F2");
+	_printTo("key_code",_keyCode.f2);
+},_keyCode.f2);
+
+_keyCustom(function(){
+	_printTo("key_name","F3");
+	_printTo("key_code",_keyCode.f3);
+},_keyCode.f3);
+
+_keyCustom(function(){
+	_printTo("key_name","F4");
+	_printTo("key_code",_keyCode.f4);
+},_keyCode.f4);
+
+_keyCustom(function(){
+	_printTo("key_name","F5");
+	_printTo("key_code",_keyCode.f5);
+},_keyCode.f5);
+
+_keyCustom(function(){
+	_printTo("key_name","F6");
+	_printTo("key_code",_keyCode.f6);
+},_keyCode.f6);
+
+_keyCustom(function(){
+	_printTo("key_name","F7");
+	_printTo("key_code",_keyCode.f7);
+},_keyCode.f7);
+
+_keyCustom(function(){
+	_printTo("key_name","F8");
+	_printTo("key_code",_keyCode.f8);
+},_keyCode.f8);
+
+_keyCustom(function(){
+	_printTo("key_name","F9");
+	_printTo("key_code",_keyCode.f9);
+},_keyCode.f9);
+
+_keyCustom(function(){
+	_printTo("key_name","F10");
+	_printTo("key_code",_keyCode.f10);
+},_keyCode.f10);
+
+_keyCustom(function(){
+	_printTo("key_name","F11");
+	_printTo("key_code",_keyCode.f11);
+},_keyCode.f11);
+
+_keyCustom(function(){
+	_printTo("key_name","F12");
+	_printTo("key_code",_keyCode.f12);
+},_keyCode.f12);
+
+_keyCustom(function(){
+	_printTo("key_name","COMMA");
+	_printTo("key_code",_keyCode.comma);
+},_keyCode.comma);
+
+_keyCustom(function(){
+	_printTo("key_name","SEMI COLON");
+	_printTo("key_code",_keyCode.semi_colon);
+},_keyCode.semi_colon);
+
+_keyCustom(function(){
+	_printTo("key_name","NUM LOCK");
+	_printTo("key_code",_keyCode.num_lock);
+},_keyCode.num_lock);
+
+_keyCustom(function(){
+	_printTo("key_name","MINUS");
+	_printTo("key_code",_keyCode.minus);
+},_keyCode.minus);
+
+_keyCustom(function(){
+	_printTo("key_name","WINDOWS MENU");
+	_printTo("key_code",_keyCode.win_menu);
+},_keyCode.win_menu);
+
+_keyCustom(function(){
+	_printTo("key_name","ADD");
+	_printTo("key_code",_keyCode.add);
+},_keyCode.add);
+
+_keyCustom(function(){
+	_printTo("key_name","DIVIDED");
+	_printTo("key_code",_keyCode.divide);
+},_keyCode.divide);
+
+_keyCustom(function(){
+	_printTo("key_name","NEXT");
+	_printTo("key_code",_keyCode.next);
+},_keyCode.next);
+
+_keyCustom(function(){
+	_printTo("key_code","PREVIOUS");
+	_printTo("key_code",_keyCode.num_lock);
+},_keyCode.previous);
