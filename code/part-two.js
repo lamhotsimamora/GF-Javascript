3771137e124f708f754c73b4bd3c707488184d12
lamhotsimamora <lamhotsimamora36@gmail.com>
2017-09-07 22:30:23 +0700

    add PUT method ajax

--- code/part-two.js
+++ code/part-two.js
@@ -1,29 +1,46 @@
 
 
 _onClick("btn_load_doc",function(){
-		_loadDoc("./server/page.html",function(r){
-		if (r)
+	_loadDoc("./server/page.html",function(a,b,c){
+		if (a)
 		{
-			_printTo("display_document",r);
+			_printTo("display_document",a);
+			_writeLog(b);
+			_writeLog(c);
 		}
-		});
+	});
 });
 
 
 var my_name = "Lorem Ipsum !";
 _onClick("btn_get",function(){
-	_requestGET("./server/profile.php?name="+my_name+"",function(r){
-		if (r)
+	_requestGET("./server/profile.php?name="+my_name+"",function(a,b,c){
+		if (a)
 		{
-			_printTo("display_get",r);
+			_printTo("display_get",a);
+			_writeLog(b);
+			_writeLog(c);
 		}
 	});
 });
 _onClick("btn_post",function(){
-	_requestPOST("./server/profile.php","name="+my_name,function(r){
-		if (r)
+	_requestPOST("./server/profile.php","name="+my_name,function(a,b,c){
+		if (a)
 		{
-			_printTo("display_post",r);
+			_printTo("display_post",a);
+			_writeLog(b);
+			_writeLog(c);
+		}
+	});
+});
+
+_onClick("btn_put",function(){
+	_requestPUT("./server/profile.php",function(a,b,c){
+		if (a)
+		{
+			_printTo("display_put",a);
+			_writeLog(b);
+			_writeLog(c);
 		}
 	});
 });
--- server/profile.php
+++ server/profile.php
@@ -8,6 +8,11 @@ if (isset($_POST['name']))
 }
 else if (isset($_GET['name']))
 {
-	$result = '<article class="message is-primary"> <div class="message-header"> <p><strong>Response From Server !</strong><a> Example Ajax with GET </a></p> <button class="delete" aria-label="delete"></button> </div> <div class="message-body"> Name Is : "'.$_GET['name'].'" </div> </article>';
+	$result = '<article class="message is-warning"> <div class="message-header"> <p><strong>Response From Server !</strong><a> Example Ajax with GET </a></p> <button class="delete" aria-label="delete"></button> </div> <div class="message-body"> Name Is : "'.$_GET['name'].'" </div> </article>';
+	echo $result;
+}
+else
+{
+	$result = '<article class="message is-success"> <div class="message-header"> <p><strong>Response From Server !</strong><a> Example Ajax </a></p> <button class="delete" aria-label="delete"></button> </div> <div class="message-body"> LOREM LOREM IPSUM </div> </article>';
 	echo $result;
 }
